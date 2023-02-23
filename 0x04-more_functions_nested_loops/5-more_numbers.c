4ebf92382ae292a47ad920877873f7feba083af2
AmineBenReddad <benreddadamine@gmail.com>
2023-02-23 06:41:02 -0800

    quick edit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,26 @@
 #include "main.h"
-/**
- * more_numbers - print the numbers 10 times
- *
- * Return: numbers
- */
 
+/**
+ * more_numbers - check the code
+ *
+ * Return: Always 0.
+ */
 void more_numbers(void)
 {
-	int i, int j;
-	int num[] = {48, 49, 50, 51, 52, 53, 54, 55, 56 ,57, 48, 49, 50, 51, 52};
+	int i;
+	int j;
+	int num[] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 50, 51, 52};
 
 	j = 0;
 	while (j < 10)
 	{
-		for (i = 0; i < 15; i++)
+		for (i = 0; i < 15 ; i++)
 		{
 			if (i > 9)
 				_putchar(49);
 		_putchar(num[i]);
 		}
 		_putchar('\n');
-		j++;
+	j++;
 	}
 }
