51915bd3104956ee95d21d1b71c7ffdff25294ba
AmineBenReddad <benreddadamine@gmail.com>
2023-03-23 10:04:31 -0700

    return sum of all parameters

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,7 +3,7 @@
 
 /*
  * File: variadic_functions.h
- * Auth: EL MAHDI BENRAMI
+ * Auth: Amine Ben Reddad
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x0F-variadic_functions directory.
  */
