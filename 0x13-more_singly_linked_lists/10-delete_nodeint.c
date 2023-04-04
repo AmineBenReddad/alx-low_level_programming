dd270af62b5fec5ee6afdae0d18f4ddfe06db439
AmineBenReddad <benreddadamine@gmail.com>
2023-04-04 10:27:08 -0700

    edit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,52 +2,35 @@
 #include <stdlib.h>
 
 /**
- * delete_nodeint_at_index - delete node at a given index
- * @head: head node
- * @index: node index
- *
- * Return: 1 (Success) | -1 (Failure)
- */
+ * delete_nodeint_at_index - Delete a node at a index positiion.
+ * @head: First node address.
+ * @index: Position of the node to delete.
+ * Return: If success (1).
+ **/
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *tmp, *del;
 	unsigned int i;
+	listint_t *current, *next;
 
-	if (!index)
-		return (del_head(head));
-
-	tmp = *head;
-	for (i = 0; i < index - 1; i++)
+	if (head == NULL || *head == NULL)
+		return (-1);
+	if (index == 0)
 	{
-		if (!tmp)
-			return (-1);
-		tmp = tmp->next;
-	}
-
-	if (tmp && tmp->next)
-	{
-		del = tmp->next;
-		tmp->next = del->next;
-		free(del);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
 		return (1);
 	}
-	return (-1);
-}
-
-/**
- * del_head - delete head node of a listint_t
- * @head: head node
- *
- * Return: 1 (Success) | -1 (Failure)
- */
-int del_head(listint_t **head)
-{
-	listint_t *tmp;
-
-	if (!(*head))
-		return (-1);
-	tmp = (*head)->next;
-	free(*head);
-	*head = tmp;
+	current = *head;
+	for (i = 0; i < index - 1; i++)
+	{
+		if (current->next == NULL)
+			return (-1);
+		current = current->next;
+	}
+	next = current->next;
+	current->next = next->next;
+	free(next);
 	return (1);
 }
